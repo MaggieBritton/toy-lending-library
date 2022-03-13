2cf00d044c320bd1d23f7dc1f63b12fd8e6c9e97
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-13 16:53:09 -0500

    Inventory Array

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -1,9 +1,254 @@
 export const INVENTORY = [
     {
         id: 0,
-        name: 
+        name: "Wooden Airplanes",
+        category: "vehicles",
+        age: ["3-6"],
+        image: "/assets/inventoryImages/airplanes.jpg",
+        description: "These three wooden airplaines in red, blue, and cream will inspire your child's imagination. A perfect addition to small-world play.",
+        checkedOut: false 
     },
     {
-
+        id: 1,
+        name: "Balance Bike",
+        category: "outdoor",
+        age: ["0-3", "3-6"],
+        image: "/assets/inventoryImages/balanceBike.jpg",
+        description: "A balance bike is the perfect way to learn steering and balance without the complication of pedals or training wheels. For ages 18 months to 5 years.",
+        checkedOut: false
     },
+    {
+        id: 2,
+        name: "Barbie",
+        category: "dolls",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/barbie.jpg",
+        description: "Dress her up, do her hair, and make up stories. Barbie is perfect for imaginary play.",
+        checkedOut: false
+    },
+    {
+        id: 3,
+        name: "Chef Costume",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/chefCostume.jpg",
+        description: "The chef costume includes a coat, hat, and kitchen utensils. Pair it with a toy kitchen and watch your little chef prepare food for everyone in the house.",
+        checkedOut: false
+    },
+    {
+        id: 4,
+        name: "Chef Costume",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/chefCostume.jpg",
+        description: "The chef costume includes a coat, hat, and kitchen utensils. Pair it with a toy kitchen and watch your little chef prepare food for everyone in the house.",
+        checkedOut: false
+    },
+    {
+        id: 5,
+        name: "Dollhouse",
+        category: "dolls",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/dollhouse.jpg",
+        description: "This large six-room dollhouse includes furniture and bendable figures.",
+        checkedOut: false
+    },
+    {
+        id: 6,
+        name: "Duplos",
+        category: "building",
+        age: ["0-3", "3-6", "6-9"],
+        image: "/assets/inventoryImages/duplos.jpg",
+        description: "Duplos are a quintessential building toy for toddlers though early gradeschoolers. They are the larger sibling of Legos and are perfect for buildling structures that don't immediately fall apart, like traditional blocks. Ages 18 months+.",
+        checkedOut: false
+    },
+    {
+        id: 7,
+        name: "Kitchen Set",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/ikeaKitchen.jpg",
+        description: "The kitchen set includes cookware, tableware, utensils, and play food. Perfect to pair with the chef's costume.",
+        checkedOut: false
+    },
+    {
+        id: 8,
+        name: "Legos",
+        category: "building",
+        age: ["6-9", "9-12"],
+        image: "/assets/inventoryImages/legos.jpg",
+        description: "Legos are the smaller sibling of Duplos and are intended for older children who can handle very small pieces. Watch your child's imagination flourish with these pint-sized building blocks.",
+        checkedOut: false
+    },
+    {
+        id: 9,
+        name: "Shapes Puzzle",
+        category: "puzzles",
+        age: ["0-3"],
+        image: "/assets/inventoryImages/loveveryPuzzle.jpg",
+        description: "This shapes puzzle features a square, triangle, and circle, and is a great introduction to puzzles for toddlers.",
+        checkedOut: false
+    },
+    {
+        id: 10,
+        name: "Ring Stacker",
+        category: "puzzles",
+        age: ["0-3"],
+        image: "/assets/inventoryImages/loveveryRingStacker.jpg",
+        description: "This ring stacker features a center post to stack the rings as well as puzzle-like slots to fit the rings around the post.",
+        checkedOut: false
+    },
+    {
+        id: 11,
+        name: "Micro Maxi Scooter",
+        category: "outdoor",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/microMaxiScooter.jpg",
+        description: "The Micro Maxi scooter will delight your child for hours. The lean-to-steer mechanism is intuitive and easy to use. The scooter also features a foot break.",
+        checkedOut: false
+    },
+    {
+        id: 12,
+        name: "Monopoly",
+        category: "games",
+        age: ["6-9", "9-12"],
+        image: "/assets/inventoryImages/monopoly.jpg",
+        description: "A classic game that has been delighting players for decades and will entertain your kids for hours.",
+        checkedOut: false
+    },
+    {
+        id: 13,
+        name: "Dump Truck",
+        category: "vehicles",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/orangeDumpTruck.jpg",
+        description: "This orange metal dump truck is perfect both indoors and outdoors in the sandbox.",
+        checkedOut: false
+    },
+    {
+        id: 14,
+        name: "Small Cars",
+        category: "vehicles",
+        age: ["3-6"],
+        image: "/assets/inventoryImages/plasticCars.jpg",
+        description: "These two small cars are the perfect size for small hands to zoom around the house.",
+        checkedOut: false
+    },
+    {
+        id: 15,
+        name: "Play Gym",
+        category: "baby",
+        age: ["0-3"],
+        image: "/assets/inventoryImages/playGym.jpg",
+        description: "This play gym is a great way to entertain baby with age-appropriate toys (a few included) that encourage reaching and grasping.",
+        checkedOut: false
+    },
+    {
+        id: 16,
+        name: "Radio Flyer Wagon",
+        category: "outdoor",
+        age: ["0-3", "3-6"],
+        image: "/assets/inventoryImages/radioFlyerWagon.jpg",
+        description: "Pull your kids along on a walk or allow older children to pull around their treasures.",
+        checkedOut: false
+    },
+    {
+        id: 17,
+        name: "Ragdoll",
+        category: "dolls",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/ragdoll.jpg",
+        description: "This cuddly doll will instantly become your child's new favorite companion.",
+        checkedOut: false
+    },
+    {
+        id: 18,
+        name: "Safari Animals",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/safariAnimals.jpg",
+        description: "The set of five safari animals includes an elepant, lion, zebra, cheeta, and rhinocerous. Perfect for small-world play.",
+        checkedOut: false
+    },
+    {
+        id: 19,
+        name: "Scrabble",
+        category: "games",
+        age: ["9-12"],
+        image: "/assets/inventoryImages/scrabble.jpg",
+        description: "A classic game of spelling with letter tiles. Sure to be a challenge for older kids.",
+        checkedOut: false
+    },
+    {
+        id: 20,
+        name: "Table with Sensory Bins",
+        category: "pretend",
+        age: ["3-6"],
+        image: "/assets/inventoryImages/sensoryBin.jpg",
+        description: "Fill the bins in this child-sized table with dry noodles, kinetic sand, dried beans, or your favorite sensory material and give your child some utensils and containers to play.",
+        checkedOut: false
+    },
+    {
+        id: 21,
+        name: "Stacking Boxes",
+        category: "puzzles",
+        age: ["0-3"],
+        image: "/assets/inventoryImages/stackingBoxes.jpg",
+        description: "Set of ten stacking boxes that also nest will provide a building and sorting challenge for your toddler. Bright images and numbers appear on the sides of the boxes.",
+        checkedOut: false
+    },
+    {
+        id: 22,
+        name: "Tractors and Trailers",
+        category: "vehicles",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/tractorsTrailers.jpg",
+        description: "Two tractors and trailers are perfect for playing farm or hauling sand around outside.",
+        checkedOut: false
+    },
+    {
+        id: 23,
+        name: "Wheelbarrow and Sand Toys",
+        category: "outdoor",
+        age: ["3-6"],
+        image: "/assets/inventoryImages/wheelbarrowSandToys.jpg",
+        description: "A wheelbarrow and sand toys are a great addition to your sandbox or for taking to the park or beach.",
+        checkedOut: false
+    },
+    {
+        id: 24,
+        name: "Wooden Animals and Trees",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/woodenAnimalsTrees.jpg",
+        description: "Three wooden animals and five trees. Pair them with blocks and make a zoo.",
+        checkedOut: false
+    },
+    {
+        id: 25,
+        name: "Wooden Blooks",
+        category: "building",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/woodenBlocks.jpg",
+        description: "These large blocks are great for small hands. Stack them up to make a tower. Build a zoo for your animals, a road for your cars, or a house for your small dolls.",
+        checkedOut: false
+    },
+    {
+        id: 26,
+        name: "Wooden Dinosaurs",
+        category: "pretend",
+        age: ["3-6", "6-9"],
+        image: "/assets/inventoryImages/woodenDinosaurs.jpg",
+        description: "A set of wooden dinosaurs will take your child's play back to prehistoric times.",
+        checkedOut: false
+    },
+    {
+        id: 27,
+        name: "Wooden Train",
+        category: "vehicles",
+        age: ["3-6", "6-9"],
+        image: "/assets/siteImages/heroTrain.jpg",
+        description: "This wooden train set is complete with train, tracks, trees, signals, and a station. Re-configure the track over and over.",
+        checkedOut: false
+    }
 ];
