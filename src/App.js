8e802067fce02f128da5c2fa381f4dc0f01650fa
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-13 22:13:34 -0500

    Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './components/Hero';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Hero />
     </div>
   );
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <p className="banner-text">Membership prices: $15/3-month trial or $50/year.</p>
+        </div>
+    );
+}
+
+export default Banner;
