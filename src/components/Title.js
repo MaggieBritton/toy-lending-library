095c80c4234fc7db68c5e2de6f9de46495765f97
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-14 21:53:31 -0500

    Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,23 @@ import { Button } from 'reactstrap';
 
 const Title = () => {
     return (
-        <div className="title-container">
-            <h1 className='title-text'>TLL</h1>
-            <Button className="login-btn" size="sm">Login</Button>
+        // <div className="title-container">
+        //     <div className="title-text-container">
+        //         <h1 className='title-text'>TLL</h1>
+        //         <Button className="login-btn" size="sm">Login</Button>
+        //     </div>
+        //     <hr className="title-hr" />
+        // </div>
+
+        <div className="container-fluid title-container">
+            <div className="row">
+                <div className="col-9 col-md-10">
+                    <h1 className='title-text'>TLL</h1>
+                </div>
+                <div className="col-3 col-md-2">
+                    <Button className="login-btn" size="sm">Login</Button>
+                </div>
+            </div>
         </div>
     );
 }
