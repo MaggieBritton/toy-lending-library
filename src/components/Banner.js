d283ab03df87a708f3adfa3d50c270b688fc40f7
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-14 21:58:26 -0500

    Banner bootstrapified

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="banner-container">
-            <p className="banner-text">Membership prices: $15/3-month trial or $50/year.</p>
+        <div className="container-fluid banner-container">
+            <div className="row">
+                <div className="col">
+                    <p className="banner-text">Membership prices: $15/3-month trial or $50/year.</p>
+                </div>
+            </div>    
         </div>
     );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,20 +3,12 @@ import { Button } from 'reactstrap';
 
 const Title = () => {
     return (
-        // <div className="title-container">
-        //     <div className="title-text-container">
-        //         <h1 className='title-text'>TLL</h1>
-        //         <Button className="login-btn" size="sm">Login</Button>
-        //     </div>
-        //     <hr className="title-hr" />
-        // </div>
-
         <div className="container-fluid title-container">
             <div className="row">
-                <div className="col-9 col-md-10">
+                <div className="col-9 col-md-10 col-xl-11">
                     <h1 className='title-text'>TLL</h1>
                 </div>
-                <div className="col-3 col-md-2">
+                <div className="col-3 col-md-2 col-xl-1">
                     <Button className="login-btn" size="sm">Login</Button>
                 </div>
             </div>
