fc5aa191d597777c2ac334b40247901212f5d513
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-17 22:57:19 -0500

    What Is CSS Small Screen Sizes

--- src/components/WhatIs.js
+++ src/components/WhatIs.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 const WhatIs = () => {
     return (
-        <div className="whatIs">
-            <img className="whatIs-image" src="assets/siteImages/TLLBear.jpg" />
-            <div className='whatIs-text'>
-                <h2>What is a Toy Lending Library?</h2>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Arcu bibendum at varius vel pharetra vel turpis nunc.
-                </p>
-                <p>
-                Pellentesque elit eget gravida cum sociis natoque. Egestas pretium aenean pharetra magna ac placerat. Donec massa sapien faucibus et molestie. Consequat mauris nunc congue nisi vitae.  
-                </p>
+        <div className="whatIs-container">
+
+            <img className="whatIs-image" src="assets/siteImages/TLLBearSquare.jpg" alt="Teddy bear wearing aviator goggles and scarf." />
+
+            <h2 className="whatIs-header">What is a Toy Lending Library?</h2>
+
+            <div className="whatIs-body">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Arcu bibendum at varius vel pharetra vel turpis nunc.
+            </p>
+            <p>
+            Pellentesque elit eget gravida cum sociis natoque. Egestas pretium aenean pharetra magna ac placerat. Donec massa sapien faucibus et molestie. Consequat mauris nunc congue nisi vitae.  
+            </p>
             </div>
+            
         </div>
     );
 }
