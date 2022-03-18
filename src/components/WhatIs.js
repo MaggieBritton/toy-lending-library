a79356f648a978ae69d079c77ab2d783c3689342
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-17 23:11:46 -0500

    What Is Responsive Layout with Bootstrap

--- src/components/WhatIs.js
+++ src/components/WhatIs.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const WhatIs = () => {
     return (
-        <div className="whatIs-container">
-
-            <img className="whatIs-image" src="assets/siteImages/TLLBearSquare.jpg" alt="Teddy bear wearing aviator goggles and scarf." />
-
-            <h2 className="whatIs-header">What is a Toy Lending Library?</h2>
-
-            <div className="whatIs-body">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Arcu bibendum at varius vel pharetra vel turpis nunc.
-            </p>
-            <p>
-            Pellentesque elit eget gravida cum sociis natoque. Egestas pretium aenean pharetra magna ac placerat. Donec massa sapien faucibus et molestie. Consequat mauris nunc congue nisi vitae.  
-            </p>
-            </div>
-            
+        <div className="container">
+            <div className="row mt-md-5">
+                <div className="col-xs-12 col-md-6">
+                    <img className="whatIs-image" src="assets/siteImages/TLLBearSquare.jpg" alt="Teddy bear wearing aviator goggles and scarf." />
+                </div>
+                <div className="col-xs-12 col-md-6 my-md-auto">
+                    <h2 className="whatIs-text">What is a Toy Lending Library?</h2>
+                    <p className="whatIs-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Arcu bibendum at varius vel pharetra vel turpis nunc.
+                    </p>
+                    <p className="whatIs-text">
+                    Pellentesque elit eget gravida cum sociis natoque. Egestas pretium aenean pharetra magna ac placerat. Donec massa sapien faucibus et molestie. Consequat mauris nunc congue nisi vitae.  
+                    </p>
+                </div>
+            </div>   
         </div>
     );
 }
