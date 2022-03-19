94b0cc005de2dbee7fcd4cb6d479a4b717d18453
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-18 23:24:05 -0500

    More Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = (props) => {
     return(
         <footer className="site-footer">
             <div className="container">
-                <div className="row">
-                    <div className="col">
+                <div className="row pt-4">
+                    <div className="col-6 col-md-4 text-center">
                         <address>
                                 Toy Lending Library <br />
                                 123 Playthings Boulevard <br />
@@ -15,7 +15,7 @@ const Footer = (props) => {
                                 <a className="footer-link" href="mailto:toylendinglibrary@email.com">Email: toylendinglibrary@email.com</a>
                         </address>
                     </div>
-                    <div className="col">
+                    <div className="col-6 col-md-4 text-center">
                         <ul className="footer-link-list">
                             <li><Link className="footer-link" to='/home'>Home</Link></li>
                             <li><Link className="footer-link" to='/toys'>Toys</Link></li>
@@ -24,7 +24,7 @@ const Footer = (props) => {
                             <li><Link className="footer-link" to='/contact'>Contact</Link></li>
                         </ul>
                     </div>
-                    <div className="col">
+                    <div className="col-12 col-md-4">
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook-square" /></a>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>  
                     </div>
@@ -38,5 +38,7 @@ const Footer = (props) => {
         </footer>
     );
 }
+
+/* Social Icons are not working...why? */
 
 export default Footer;
