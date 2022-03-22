9c91ba3b22c0da5f9cd87cc7477fee84022c90d8
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-22 00:15:35 -0500

    Featured Toys Cards & Styling

--- src/components/FeaturedToys.js
+++ src/components/FeaturedToys.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Card, CardImg } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, CardText, CardDeck, Button } from 'reactstrap';
 
 const RenderFeaturedToys = ({inventoryItems}) => {
-        return(
-            <div>{inventoryItems.map(item => 
-                <Card key={item.id}>
-                    <CardImg src={item.image} alt={item.name} />
-                </Card> )}
-            </div>
-        );
-}
+    
+    return(
+        <div className="row">{inventoryItems.map(item => 
+            <div className="col-xs-12 col-sm-6 col-lg-3">
+                <Card className="featuredToy-card mb-3" key={item.id}>
+                    <CardImg className="featuredToy-image" top src={item.image} alt={item.name} />
+                    <CardImgOverlay className="featuredToy-overlay text-center">
+                        <CardTitle className="featuredToy-title">{item.name}</CardTitle>
+                        <CardText className="featuredToy-text">
+                            {item.category}
+                            <br />
+                            ages {item.age}
+                        </CardText>
+                        <Button className="featuredToy-btn">View Details</Button>
+                    </CardImgOverlay>
+                </Card> 
+            </div>)}
+        </div>
+    );
+};
 
 const FeaturedToys = (props)=> {
     return(
@@ -19,13 +31,8 @@ const FeaturedToys = (props)=> {
                     <h2 className="text-center">Featured Toys</h2>
                 </div>
             </div>
-            <div className="row">
-                <div className="col">
-                    <RenderFeaturedToys inventoryItems={props.inventoryItems} />
-                </div>
-            </div>
-        </div> 
-        
+            <RenderFeaturedToys inventoryItems={props.inventoryItems} />
+        </div>    
     );
 }
 
