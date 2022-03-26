f3c7220403979839c24871af77ecdf304125b4b9
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-25 23:25:11 -0500

    Inventory Filter Buttons Setup

--- src/components/FeaturedToys.js
+++ src/components/FeaturedToys.js
@@ -1,30 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardTitle, CardText, Button, CardImgOverlay } from 'reactstrap';
-
-const RenderFeaturedToys = ({inventoryItems}) => {
-    
-
-    return(
-        <div className="row">{inventoryItems.map(item => 
-            <div className="col-xs-12 col-sm-6 col-lg-3">
-                <Card className="featuredToy-card mb-3" key={item.id}>
-                    <CardImg id="featuredToy-image" className="featuredToy-image" top src={item.image} alt={item.name} />
-                    <CardImgOverlay id="featuredToy-overlay" className="featuredToy-overlay text-center">
-                        <div className="featuredToy-allText">
-                            <CardTitle tag="h4" className="featuredToy-title">{item.name}</CardTitle>
-                            <CardText className="featuredToy-text">
-                                {item.category}
-                                <br />
-                                ages {item.age.join(", ")}
-                            </CardText>
-                            <Button className="featuredToy-btn">View Details</Button>
-                        </div>
-                    </CardImgOverlay>
-                </Card> 
-            </div>)}
-        </div>
-    );
-};
+import { Button } from 'reactstrap';
+import InventoryCard from './InventoryCard';
 
 const FeaturedToys = (props)=> {
     return(
@@ -34,7 +10,7 @@ const FeaturedToys = (props)=> {
                     <h2 className="text-center my-4">Featured Toys</h2>
                 </div>
             </div>
-            <RenderFeaturedToys inventoryItems={props.inventoryItems} />
+            <InventoryCard inventoryItems={props.inventoryItems} />
             <div className="row">
                 <div className="col text-center">
                     <Button className="button my-4">More Toys</Button>
--- src/components/Toys.js
+++ src/components/Toys.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InventoryCard from './InventoryCard';
+import { Button } from 'reactstrap';
 
 const Toys = (props)=> {
     return(
@@ -12,7 +13,40 @@ const Toys = (props)=> {
                     <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Trucklk"/>
                 </div>
             </div>
-            <InventoryCard inventoryItems={props.inventoryItems}/> 
+            <div className="row">
+                <div className="col-12 col-md-4 col-lg-3 mt-4 ml-2">
+                    <div className="filter-heading-div mr-2">
+                        <h3 className="filter-heading">Filters</h3>
+                        <a>clear all</a>   
+                    </div>
+                    
+                    
+                    <h4 className="filter-heading">Category</h4>
+                    <Button outline className="filter-btn">building</Button>
+                    <Button outline className="filter-btn">dolls</Button>
+                    <Button outline className="filter-btn">games</Button>
+                    <Button outline className="filter-btn">pretend</Button>
+                    <Button outline className="filter-btn">puzzles</Button>
+                    <Button outline className="filter-btn">outdoor</Button>
+                    <Button outline className="filter-btn">vehicles</Button>
+
+                    <h4 className="filter-heading">Age</h4>
+                    <Button outline className="filter-btn">0-3</Button>
+                    <Button outline className="filter-btn">3-6</Button>
+                    <Button outline className="filter-btn">6-9</Button>
+                    <Button outline className="filter-btn">9-12</Button>
+
+                    <h4 className="filter-heading">Availability</h4>
+                    <Button outline className="filter-btn">available</Button>
+                    <Button outline className="filter-btn">checked out</Button>
+
+
+                </div>
+                <div className="col">
+                    <InventoryCard inventoryItems={props.inventoryItems}/> 
+                </div>
+            </div>
+            
         </div>  
     )
 };
@@ -24,7 +58,6 @@ export default Toys;
 
 To Do:
 Add filters to Toy Inventory Page
-Extract <Card> into its own component that Featured Toys and Toys (Inventory) share
 Code individual toy pages
 Code About Us, Sign Up, Contact pages
 Add animation to cards when hovered on (slight wiggle or scale?)
