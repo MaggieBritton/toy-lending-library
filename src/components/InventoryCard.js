d66f8fbbd255eaec77a076b73fe6005b97f4eee2
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-25 22:43:17 -0500

    Inventory Card Extraction into Own Component

---
+++ src/components/InventoryCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, CardImg, CardTitle, CardText, Button, CardImgOverlay } from 'reactstrap';
+
+const InventoryCard = ({inventoryItems}) => {
+    return (
+        <div className="row mt-4">{inventoryItems.map(item => 
+            <div className="col-xs-12 col-sm-6  col-md-4 col-lg-3"> 
+                <Card className="inventory-card mb-3" key={item.id}>
+                    <CardImg id="inventoryItem-image" className="inventoryItem-image" top src={item.image} alt={item.name} />
+                    <CardImgOverlay id="inventoryItem-overlay" className="inventoryItem-overlay text-center">
+                        <div className="inventoryItem-allText">
+                            <CardTitle tag="h4" className="inventoryItem-title">{item.name}</CardTitle>
+                            <CardText className="inventoryItem-text">
+                                {item.category}
+                                <br />
+                                ages {item.age.join(", ")}
+                            </CardText>
+                            <Button className="inventoryItem-btn">View Details</Button>
+                        </div>
+                    </CardImgOverlay>
+                </Card>
+            </div>)}
+        </div>
+    );
+};
+
+export default InventoryCard;
--- src/components/Toys.js
+++ src/components/Toys.js
@@ -1,30 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardTitle, CardText, Button, CardImgOverlay } from 'reactstrap';
-
-const RenderToys = ({inventoryItems}) => {
-    
-
-    return(
-        <div className="row mt-4">{inventoryItems.map(item => 
-            <div className="col-xs-12 col-sm-6 col-lg-3">
-                <Card className="featuredToy-card mb-3" key={item.id}>
-                    <CardImg id="featuredToy-image" className="featuredToy-image" top src={item.image} alt={item.name} />
-                    <CardImgOverlay id="featuredToy-overlay" className="featuredToy-overlay text-center">
-                        <div className="featuredToy-allText">
-                            <CardTitle tag="h4" className="featuredToy-title">{item.name}</CardTitle>
-                            <CardText className="featuredToy-text">
-                                {item.category}
-                                <br />
-                                ages {item.age.join(", ")}
-                            </CardText>
-                            <Button className="featuredToy-btn">View Details</Button>
-                        </div>
-                    </CardImgOverlay>
-                </Card> 
-            </div>)}
-        </div>
-    );
-}; 
+import InventoryCard from './InventoryCard';
 
 const Toys = (props)=> {
     return(
@@ -37,9 +12,9 @@ const Toys = (props)=> {
                     <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Trucklk"/>
                 </div>
             </div>
-            <RenderToys inventoryItems={props.inventoryItems} />
-        </div>    
-    );
+            <InventoryCard inventoryItems={props.inventoryItems}/> 
+        </div>  
+    )
 };
 
 
@@ -52,5 +27,6 @@ Add filters to Toy Inventory Page
 Extract <Card> into its own component that Featured Toys and Toys (Inventory) share
 Code individual toy pages
 Code About Us, Sign Up, Contact pages
+Add animation to cards when hovered on (slight wiggle or scale?)
 
 */
