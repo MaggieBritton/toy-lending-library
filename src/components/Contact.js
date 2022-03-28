97c9e94e7ef6ad1a0005e5861b4546d0e27636e7
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-27 23:34:12 -0500

    Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,193 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Col, Button, FormFeedback } from 'reactstrap';
 
 class Contact extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+            contactType: 'By Email',
+            message: '',
+            touched: {
+                firstName: false,
+                lastName: false,
+                phoneNum: false,
+                email: false
+            }
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('Current state is: ' + JSON.stringify(this.state));
+        alert('Current state is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    validate(firstName, lastName, phoneNum, email) {
+
+        const errors = {
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: ''
+        };
+
+        if (this.state.touched.firstName) {
+            if (firstName.length < 2) {
+                errors.firstName = 'First name must be at least 2 characters.';
+            } else if (firstName.length > 15) {
+                errors.firstName = 'First name must be 15 characters or fewer.';
+            }
+        }
+
+        if (this.state.touched.lastName) {
+            if (lastName.length < 2) {
+                errors.lastName = 'Last name must be at least 2 characters.';
+            } else if (lastName.length > 15) {
+                errors.lastName = 'Last name must be 15 characters or fewer.';
+            }
+        }
+
+        const reg = /^\d+$/;
+        if (this.state.touched.phoneNum && !reg.test(phoneNum)) {
+            errors.phoneNum = 'The phone number should contain only numbers.';
+        }
+
+        const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/;
+        if (this.state.touched.email && !emailReg.test(email)) {
+            errors.email = 'Invalid email.';
+        }
+
+        return errors;
+    }
+
+    handleBlur = (field) => () => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true}
+        });
     }
 
     render() {
+        
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.phoneNum, this.state.email);
+        
         return(
-            <div className="container-fluid">
-                <div className="row contact-bg">
-                    <div className="col my-auto">
-                        <h1 className="text-center">Contact</h1>
-                    </div>
-                    <div className="col pr-0">
-                        <img className="contact-img" src="assets/siteImages/confusedBunnySquare.jpg" alt="Confused Bunny"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div class="col">
-                        <Form>
-                            
-                        </Form>
+            <React.Fragment>
+                <div className="container-fluid">
+                    <div className="row contact-bg">
+                        <div className="col my-auto">
+                            <h1 className="text-center">Contact</h1>
+                        </div>
+                        <div className="col pr-0">
+                            <img className="contact-img" src="assets/siteImages/confusedBunnySquare.jpg" alt="Confused Bunny"/>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h2>Contact Us</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div class="col">
+                            <p>Please let us know if you have any questions! We will get back to you via email or phone as soon as we can.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div class="col">
+                            <Form onSubmit={this.handleSubmit}>
+                                <FormGroup row>
+                                    <Label htmlFor="firstName" md={2}>First Name</Label>
+                                    <Col md={10} lg={8}>
+                                        <Input required type="text" id="firstName" name="firstName" placeholder="First Name"
+                                        value={this.state.firstName}
+                                        invalid={errors.firstName}
+                                        onBlur={this.handleBlur("firstName")}
+                                        onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.firstName}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="lastName" md={2}>Last Name</Label>
+                                    <Col md={10} lg={8}>
+                                        <Input required type="text" id="lastName" name="lastName" placeholder="Last Name"
+                                        value={this.state.lastName}
+                                        invalid={errors.lastName}
+                                        onBlur={this.handleBlur("lastName")}
+                                        onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.lastName}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="phoneNum" md={2}>Email</Label>
+                                    <Col md={10} lg={8}>
+                                        <Input required type="tel" id="phoneNum" name="phoneNum" placeholder="Phone Number"
+                                        value={this.state.phoneNum}
+                                        invalid={errors.phoneNum}
+                                        onBlur={this.handleBlur("phoneNum")}
+                                        onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.phoneNum}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="email" md={2}>Email</Label>
+                                    <Col md={10} lg={8}>
+                                        <Input type="email" id="email" name="email" placeholder="Email"
+                                        value={this.state.email}
+                                        invalid={errors.email}
+                                        onBlur={this.handleBlur("email")}
+                                        onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.email}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="message" md={2}>Message</Label>
+                                    <Col md={10} lg={8}>
+                                        <Input type="textarea" id="message" name="message" rows="10"
+                                        value={this.state.message}
+                                        onChange={this.handleInputChange}></Input>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Col md={{size: 10, offset: 2}}>
+                                        <Button type="submit" color="warning">
+                                            Submit
+                                        </Button>
+                                    </Col>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
 
 export default Contact;
+
+/*
+To Do:
+how can I make the form clear when the submit button is hit?
+use a more sophisticated regex for the phone number?
+
+
+*/
--- src/components/Toys.js
+++ src/components/Toys.js
@@ -4,50 +4,53 @@ import { Button } from 'reactstrap';
 
 const Toys = (props)=> {
     return(
-        <div className="container-fluid">
-            <div className="row toyInventory-bg">
-                <div className="col my-auto">
-                    <h2 className="text-center">Toy Inventory</h2>
-                </div>
-                <div className="col  pr-0">
-                    <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Truck"/>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12 col-md-4 col-lg-3 mt-4 ml-2">
-                    <div className="filter-heading-div mr-2">
-                        <h3 className="filter-heading">Filters</h3>
-                        <Button outline color="danger" className="btn-sm filter-btn">clear all</Button>   
+        <React.Fragment>
+            <div className="container-fluid">
+                <div className="row toyInventory-bg">
+                    <div className="col my-auto">
+                        <h2 className="text-center">Toy Inventory</h2>
                     </div>
-                    
-                    
-                    <h4 className="filter-heading">Category</h4>
-                    <Button outline className="filter-btn">building</Button>
-                    <Button outline className="filter-btn">dolls</Button>
-                    <Button outline className="filter-btn">games</Button>
-                    <Button outline className="filter-btn">pretend</Button>
-                    <Button outline className="filter-btn">puzzles</Button>
-                    <Button outline className="filter-btn">outdoor</Button>
-                    <Button outline className="filter-btn">vehicles</Button>
-
-                    <h4 className="filter-heading">Age</h4>
-                    <Button outline className="filter-btn">0-3</Button>
-                    <Button outline className="filter-btn">3-6</Button>
-                    <Button outline className="filter-btn">6-9</Button>
-                    <Button outline className="filter-btn">9-12</Button>
-
-                    <h4 className="filter-heading">Availability</h4>
-                    <Button outline className="filter-btn">available</Button>
-                    <Button outline className="filter-btn">checked out</Button>
-
-
-                </div>
-                <div className="col">
-                    <InventoryCard inventoryItems={props.inventoryItems}/> 
+                    <div className="col  pr-0">
+                        <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Truck"/>
+                    </div>
                 </div>
             </div>
-            
-        </div>  
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-4 col-lg-3 mt-4">
+                        <div className="filter-heading-div">
+                            <h3 className="filter-heading">Filters</h3>
+                            <Button outline color="danger" className="btn-sm filter-btn">clear all</Button>   
+                        </div>
+                        
+                        
+                        <h4 className="filter-heading">Category</h4>
+                        <Button outline className="filter-btn">building</Button>
+                        <Button outline className="filter-btn">dolls</Button>
+                        <Button outline className="filter-btn">games</Button>
+                        <Button outline className="filter-btn">pretend</Button>
+                        <Button outline className="filter-btn">puzzles</Button>
+                        <Button outline className="filter-btn">outdoor</Button>
+                        <Button outline className="filter-btn">vehicles</Button>
+
+                        <h4 className="filter-heading">Age</h4>
+                        <Button outline className="filter-btn">0-3</Button>
+                        <Button outline className="filter-btn">3-6</Button>
+                        <Button outline className="filter-btn">6-9</Button>
+                        <Button outline className="filter-btn">9-12</Button>
+
+                        <h4 className="filter-heading">Availability</h4>
+                        <Button outline className="filter-btn">available</Button>
+                        <Button outline className="filter-btn">checked out</Button>
+
+
+                    </div>
+                    <div className="col">
+                        <InventoryCard inventoryItems={props.inventoryItems}/> 
+                    </div>
+                </div>
+            </div> 
+        </React.Fragment> 
     )
 };
 
