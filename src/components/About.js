274ada4ef9df624d72a6b8c6864343fa2b4f69da
Margaret Britton <margaret.ann.britton@gmail.com>
2022-03-30 09:40:28 -0500

    About Us Images & Tab Setup

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames'; 
 
-const About = () => {
-    return (
-        <React.Fragment>
-            <div className="container-fluid">
-                <div className="row about-bg">
-                    <div className="col my-auto">
-                        <h1 className="text-center">About</h1>
-                    </div>
-                    <div className="col pr-0">
-                        <img className="about-img" src="assets/siteImages/playBlocks.jpg" alt="Blocks with Letters Spelling Play"/>
+class About extends Component {
+    constructor (props) {
+        super(props);
+
+        this.toggle=this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container-fluid">
+                    <div className="row about-bg">
+                        <div className="col my-auto">
+                            <h1 className="text-center">About</h1>
+                        </div>
+                        <div className="col pr-0">
+                            <img className="about-img" src="assets/siteImages/playBlocks.jpg" alt="Blocks with Letters Spelling Play"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h2>About Us</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis aliquam faucibus purus in massa tempor. Mi proin sed libero enim sed faucibus turpis. In tellus integer feugiat scelerisque varius morbi enim nunc. Aliquam ultrices sagittis orci a. Massa placerat duis ultricies lacus sed turpis tincidunt id aliquet. Morbi tincidunt augue interdum velit euismod. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Ultrices eros in cursus turpis massa tincidunt dui ut. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Posuere morbi leo urna molestie at.</p>
-                    <p>Odio eu feugiat pretium nibh ipsum consequat nisl vel. Ultrices tincidunt arcu non sodales. Erat nam at lectus urna duis convallis convallis tellus id. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Facilisis gravida neque convallis a cras. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Non tellus orci ac auctor augue mauris augue. Ultrices neque ornare aenean euismod elementum. Suspendisse interdum consectetur libero id faucibus. Gravida dictum fusce ut placerat orci.</p>    
-
-                    <p>Convallis a cras semper auctor. Viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas. Bibendum est ultricies integer quis auctor elit sed vulputate. Ut porttitor leo a diam sollicitudin tempor id eu. Egestas congue quisque egestas diam in arcu. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. Ut tortor pretium viverra suspendisse potenti nullam. Dolor sit amet consectetur adipiscing elit ut. Urna duis convallis convallis tellus id interdum velit. Amet commodo nulla facilisi nullam vehicula. Nisl rhoncus mattis rhoncus urna neque viverra justo nec.</p>
-
-                    <p>Consequat mauris nunc congue nisi. Erat nam at lectus urna. Lorem dolor sed viverra ipsum. Scelerisque eleifend donec pretium vulputate. Nibh tellus molestie nunc non blandit massa enim nec. Sed tempus urna et pharetra. Sed vulputate odio ut enim. Pellentesque habitant morbi tristique senectus et netus et malesuada. Morbi leo urna molestie at elementum eu facilisis sed. Arcu dui vivamus arcu felis. A scelerisque purus semper eget duis. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Vestibulum sed arcu non odio euismod lacinia at.</p>
+                <div className="container">
+                    <div className="row my-4">
+                        <div className="col">
+                            <h2>About Us</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis aliquam faucibus purus in massa tempor. Mi proin sed libero enim sed faucibus turpis. In tellus integer feugiat scelerisque varius morbi enim nunc. Aliquam ultrices sagittis orci a. Massa placerat duis ultricies lacus sed turpis tincidunt id aliquet. Morbi tincidunt augue interdum velit euismod. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Ultrices eros in cursus turpis massa tincidunt dui ut. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Posuere morbi leo urna molestie at.</p>
+                            <p>Odio eu feugiat pretium nibh ipsum consequat nisl vel. Ultrices tincidunt arcu non sodales. Erat nam at lectus urna duis convallis convallis tellus id. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Facilisis gravida neque convallis a cras. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Non tellus orci ac auctor augue mauris augue. Ultrices neque ornare aenean euismod elementum. Suspendisse interdum consectetur libero id faucibus. Gravida dictum fusce ut placerat orci.</p>    
+                        </div>
                     </div>
-                </div> 
-            </div>
-        </React.Fragment>
-    );
+                    <div className="row">
+                        <Nav tabs>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '1' })}
+                                    onClick={() => { this.toggle('1'); }}
+                                >
+                                  Tab 1  
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '2' })}
+                                    onClick={() => { this.toggle('2'); }}
+                                >
+                                  Tab 2  
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '3' })}
+                                    onClick={() => { this.toggle('3'); }}
+                                >
+                                    Tab 3  
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        <TabContent >
+
+                        </TabContent>
+                    </div> 
+                </div>
+            </React.Fragment>
+        );
+    }
+    
 };
 
 export default About;
 
 /* Add:
-carousel with images of children playing
+tabs with info about the TLL (include photo in each tab view)
 
 */
