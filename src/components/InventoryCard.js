5361c60b2cfc645c1f6b20f6dbb207e779319be9
James R. Maxfield <Vercjames@gmail.com>
2022-04-10 18:07:57 -0500

    filter function

--- src/components/InventoryCard.js
+++ src/components/InventoryCard.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { Card, CardImg, CardTitle, Button, CardImgOverlay } from 'reactstrap';
 import { useHistory } from 'react-router-dom';
 
-const InventoryCard = ({inventoryItems}) => {
-    
+const InventoryCard = ({item}) => {
+
     const history = useHistory();
 
     function handleClick(path) {
         history.push(path);
     }
-    
+
     return (
-        <div className="row mt-4">{inventoryItems.map(item => 
-            <div key={item.id} className="col-xs-12 col-sm-6 col-lg-3"> 
-                <Card className="inventoryItem-card mb-3" >
-                    <CardImg id="inventoryItem-image" className="inventoryItem-image" top src={item.image} alt={item.name} />
-                    <CardImgOverlay id="inventoryItem-overlay" className="inventoryItem-overlay text-center">
-                        <div className="inventoryItem-allText">
-                            <CardTitle tag="h4" className="inventoryItem-title">{item.name}</CardTitle>
-                            <Button className="inventoryItem-btn" onClick={()=> handleClick(`/toys/${item.id}`)}>View Details</Button>
-                        </div>
-                    </CardImgOverlay>
-                </Card>
-            </div>)}
-        </div>
+      <div key={item.id} className="col-xs-12 col-sm-6 col-lg-3">
+          <Card className="inventoryItem-card mb-3" >
+              <CardImg id="inventoryItem-image" className="inventoryItem-image" top src={item.image} alt={item.name} />
+              <CardImgOverlay id="inventoryItem-overlay" className="inventoryItem-overlay text-center">
+                  <div className="inventoryItem-allText">
+                      <CardTitle tag="h4" className="inventoryItem-title">{item.name}</CardTitle>
+                      <Button className="inventoryItem-btn" onClick={()=> handleClick(`/toys/${item.id}`)}>View Details</Button>
+                  </div>
+              </CardImgOverlay>
+          </Card>
+      </div>
     );
 };
 
--- src/components/Toys.js
+++ src/components/Toys.js
@@ -3,83 +3,130 @@ import InventoryCard from './InventoryCard';
 import { Button } from 'reactstrap';
 
 const Toys = ({ inventoryItems })=> {
+    const [filterAge, setFilterAge] = useState(null)
+    const [filterStatus, setFilterStatus] = useState(false)
+    const [filterCategory, setFilterCategory] = useState(null)
 
-    const [items, setItems] = useState(inventoryItems);
-
-    const filterCategory = (category) => {
-        const filteredCategoryResult = inventoryItems.filter(currentItem => {
-            return currentItem.category === category;
-        });
-        setItems(filteredCategoryResult);
-    }
-    //filterAge function still isn't accessing age data because it's nested in its own array
-    const filterAge = (age) => {
-        const filteredAgeResult = inventoryItems.filter(currentItem => currentItem.age.includes(age));
-        setItems(filteredAgeResult);
+    const resetFilter = () => {
+        setFilterAge(null)
+        setFilterStatus(false)
+        setFilterCategory(null)
     }
 
-    const filterStatus = (status) => {
-        const filteredStatusResult = inventoryItems.filter(currentItem => {
-            return currentItem.checkedOut === status;
-        });
-        setItems(filteredStatusResult);
-    }
 
     return(
-        <React.Fragment>
-            <div className="container-fluid">
-                <div className="row toyInventory-bg">
-                    <div className="col my-auto">
-                        <h2 className="text-center">Toy Inventory</h2>
-                    </div>
-                    <div className="col  pr-0">
-                        <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Truck"/>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-4 col-lg-3 mt-4">
-                        <div className="filter-heading-div">
-                            <h3 className="filter-heading">Filters</h3>
-                            <Button outline color="danger" className="btn-sm filter-btn" onClick={()=>{setItems(inventoryItems)}}>clear all</Button>   
-                        </div>
-                        
-                        
-                        <h4 className="filter-heading">Category</h4>
-                        <Button outline className="filter-btn" onClick={() => filterCategory('building')}>building</Button>
-                        <Button outline className="filter-btn"onClick={() => filterCategory('dolls')}>dolls</Button>
-                        <Button outline className="filter-btn" onClick={() => filterCategory('games')}>games</Button>
-                        <Button outline className="filter-btn"onClick={() => filterCategory('pretend')}>pretend</Button>
-                        <Button outline className="filter-btn" onClick={() => filterCategory('puzzles')}>puzzles</Button>
-                        <Button outline className="filter-btn"onClick={() => filterCategory('outdoor')}>outdoor</Button>
-                        <Button outline className="filter-btn" onClick={() => filterCategory('vehicles')}>vehicles</Button>
+      <React.Fragment>
+          <div className="container-fluid">
+              <div className="row toyInventory-bg">
+                  <div className="col my-auto">
+                      <h2 className="text-center">Toy Inventory</h2>
+                  </div>
+                  <div className="col  pr-0">
+                      <img className="inventory-img" src="assets/siteImages/orangeDumpTruck.jpg" alt="Orange Metal Dump Truck"/>
+                  </div>
+              </div>
+          </div>
+          <div className="container">
+              <div className="row">
+                  <div className="col-12 col-md-4 col-lg-3 mt-4">
+                      <div className="filter-heading-div">
+                          <h3 className="filter-heading">Filters</h3>
+                          <Button outline color="danger" className="btn-sm filter-btn" onClick={()=>resetFilter()}>clear all</Button>
+                      </div>
 
-                        <h4 className="filter-heading">Age</h4>
-                        <Button outline className="filter-btn" onClick={()=>{filterAge('0-3')}}>0-3</Button>
-                        <Button outline className="filter-btn" onClick={()=>{filterAge('3-6')}}>3-6</Button>
-                        <Button outline className="filter-btn" onClick={()=>{filterAge('6-9')}}>6-9</Button>
-                        <Button outline className="filter-btn" onClick={()=>{filterAge('9-12')}}>9-12</Button>
+                      <h4 className="filter-heading">Category</h4>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "building" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('building')}>
+                          building
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "dolls" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('dolls')}>
+                          dolls
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "games" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('games')}>
+                          games
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "pretend" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('pretend')}>
+                          pretend
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "puzzles" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('puzzles')}>
+                          puzzles
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "outdoor" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('outdoor')}>
+                          outdoor
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterCategory === "vehicles" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterCategory('vehicles')}>
+                          vehicles
+                      </button>
 
-                        <h4 className="filter-heading">Status</h4>
-                        <Button outline className="filter-btn" onClick={()=>{filterStatus(false)}}>available</Button>
-                        <Button outline className="filter-btn" onClick={()=>{filterStatus(true)}}>checked out</Button>
+                      <h4 className="filter-heading">Age</h4>
+                      <button
+                        className={`filter-btn btn ${filterAge === "0-3" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterAge('0-3')}>
+                          0-3
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterAge === "3-6" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterAge('3-6')}>
+                          3-6
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterAge === "6-9" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterAge('6-9')}>
+                          6-9
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterAge === "9-12" ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterAge('9-12')}>
+                          9-12
+                      </button>
 
+                      <h4 className="filter-heading">Status</h4>
+                      <button
+                        className={`filter-btn btn ${filterStatus === false ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterStatus(false)}>
+                          Available
+                      </button>
+                      <button
+                        className={`filter-btn btn ${filterStatus === true ? "btn-primary" : "btn-outline-secondary"}`}
+                        onClick={() => setFilterStatus(true)}>
+                          Checked Out
+                      </button>
+                  </div>
+                  <div className="col">
+                      <div className="row">
+                          {inventoryItems.map((item, index) => {
+                              if (filterStatus !== item.checkedOut) return null
+                              if (filterCategory !== null && filterCategory !== item.category) return null
+                              if (filterAge !== null && !item.age.includes(filterAge)) return null
 
-                    </div>
-                    <div className="col">
-                        <InventoryCard inventoryItems={items}/> 
-                    </div>
-                </div>
-            </div> 
-        </React.Fragment> 
+                              return (
+                                <InventoryCard key={index} item={item}/>
+                              )
+                          })}
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </React.Fragment>
     )
 };
 
-
 export default Toys;
 
-/* 
+/*
 
 To Do:
 Add filters to Toy Inventory Page
